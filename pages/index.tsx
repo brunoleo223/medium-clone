c34b3e1d281ce53abc05c41a1c6d15c1167c8912
brunoleo223 <brunoleo223@gmail.com>
2022-05-28 17:08:01 -0300

    listing post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import Header from '../components/Header';
 import { sanityClient, urlFor } from "../sanity"
 import { Post } from '../typings';
@@ -9,8 +10,6 @@ interface PropsData {
 }
 
 function Home({ posts }: PropsData) {
-
-  console.log(posts)
   
   return ( 
     <div className="max-w-7xl mx-auto">
@@ -31,6 +30,23 @@ function Home({ posts }: PropsData) {
         <div>
           <img className='hidden md:inline-flex h-32 lg:h-full' src="https://iconape.com/wp-content/png_logo_vector/medium-m.png" alt="" />
         </div>
+      </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='group cursor-pointer border rounded-lg overflow-hidden'>
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform ease-in-out' src={urlFor(post.mainImage).url()} alt="" />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-sm'>{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()} alt="" />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
