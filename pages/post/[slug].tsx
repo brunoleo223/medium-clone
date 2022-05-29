990f45fc489e225eca97f4778a48b600e1a95bba
brunoleo223 <brunoleo223@gmail.com>
2022-05-29 17:18:30 -0300

    create comments feature

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,12 +3,41 @@ import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity"
 import { Post } from '../../typings';
 import PortableText from 'react-portable-text';
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from "react";
 
 interface PropsData {
     post: Post;
   }
 
+interface FormData{
+    _id: string;
+    name: string;
+    email: string;
+    comment: string;
+}
+
 function Post({ post }: PropsData) {
+
+    console.log(post.comments)
+
+    const [submitted, setSubmitted] = useState(false);
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+
+    const onSubmit:SubmitHandler<FormData> = (data) => {
+        fetch('/api/createComment', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log(data)
+            setSubmitted(true)
+        }).catch((err) => {
+            console.log(err)
+            setSubmitted(false)
+        })
+    }
+
   return (
     <main>
         <Header />
@@ -26,8 +55,6 @@ function Post({ post }: PropsData) {
                 </p>
             </div>
 
-            
-
             <div className="mt-10">
                 <PortableText
                     content={post.body}
@@ -39,7 +66,7 @@ function Post({ post }: PropsData) {
                                 <h1 className="text-2xl font-bold mt-5" {...props} />
                             ),
                             h2: (props: any) => (
-                                <h2 className="text-xl font-bold mt-5" {...props} />
+                                <h2 className="text-xl font-bold my-5" {...props} />
                             ),
                             li: ({children}: any) => (
                                 <li className="ml-4 list-disc">{children}</li>
@@ -54,6 +81,56 @@ function Post({ post }: PropsData) {
                 />
             </div>
         </article>
+        <hr className="max-w-lg mt-5 mx-auto border border-yellow-500" />
+
+        {submitted ? (
+            <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+                <h3 className="text-3xl font-bold">
+                    Thank you for submitting your comment!
+                </h3>
+                <p>
+                    Once it has been approved, it will appear below!
+                </p>
+                <button className="bg-white text-yellow-500 font-bold rounded p-3 mt-3" onClick={() => setSubmitted(false)}>Leave an other comment</button>
+            </div>
+        ) : (
+            <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10" onSubmit={handleSubmit(onSubmit)}> 
+            <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+            <h4 className="text-3xl font-bold">Leave a comment below</h4>
+
+                <input type="hidden" name="_id" value={post._id} {...register("_id")} />
+
+                <label htmlFor="" className="block mb-5">
+                    <span className="text-gray-700">Name</span>
+                    <input {...register("name", {required: true})} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" type="text" />
+                </label>
+                <label htmlFor="" className="block mb-5">
+                    <span className="text-gray-700">Email</span>
+                    <input {...register("email", {required: true})} className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" type="text" />
+                </label>
+                <label htmlFor="" className="block mb-5">
+                    <span className="text-gray-700">Comment</span>
+                    <textarea {...register("comment", {required: true})} className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" rows={4}></textarea>
+                </label>
+                <div className="flex flex-col p-5">
+                    {errors.name && <span className="text-red-500">- The Name Field is required</span>}
+                    {errors.comment && <span className="text-red-500">- The Comment Field is required</span>}
+                    {errors.email && <span className="text-red-500">- The Email Field is required</span>}
+                </div>
+                <input type="submit" className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" />
+            </form>
+        )}
+
+        <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-x-2">
+            <h3 className="text-4xl pb-3">Comments</h3>
+            <hr className="pb-3" />
+            {post.comments.map((comment) => (
+                <div key={comment._id}>
+                    <p className="mb-3"><span className="text-yellow-500">{comment.name}:</span> {comment.comment}</p>
+                </div>
+            ))}
+        </div>
+
     </main>
   )
 }
